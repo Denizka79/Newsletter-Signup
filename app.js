dcfba47d3bda2e15dd6e499a12f141b84ae459d1
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2021-11-19 18:38:50 +0300

    Add app boilerplate

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-1 
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
+});
